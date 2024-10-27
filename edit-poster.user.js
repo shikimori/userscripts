5c3eddd73a13471bffde2cf43fe74f1012d5f2f8
Andrey Sidorov <takandar@gmail.com>
2024-10-27 21:10:19 +0300

    update url match conditions

--- edit-poster.user.js
+++ edit-poster.user.js
@@ -20,7 +20,7 @@
     ready();
 
     function ready() {
-        if (!window.location.pathname.startsWith('/animes/') && !window.location.pathname.startsWith('/mangas/') && !window.location.pathname.startsWith('/ranobe/')) { return; }
+        if (!window.location.pathname.match(/\/(?:animes|mangas|ranobe|characters|people)\//')) { return; }
 
         const $container = $('.b-subposter-actions');
         if (!$container.length) { return; }
