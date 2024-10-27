cb3c09756f585f964ea7591c1ee5ab89a8953f77
Andrey Sidorov <takandar@gmail.com>
2024-10-27 21:14:19 +0300

    update urls

--- edit-fandubbers.user.js
+++ edit-fandubbers.user.js
@@ -7,8 +7,8 @@
 // @match        *://shikimori.local/*
 // @match        *://shikimori.one/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shikimori.one
-// @downloadURL  https://github.com/shikimori/userscripts/blob/master/edit-fandubbers.user.js
-// @updateURL    https://github.com/shikimori/userscripts/raw/refs/heads/master/edit-fandubbers.user.js
+// @downloadURL  https://raw.githubusercontent.com/shikimori/userscripts/refs/heads/master/edit-external_links.user.js
+// @updateURL    https://raw.githubusercontent.com/shikimori/userscripts/refs/heads/master/edit-external_links.user.js
 // @grant        none
 // ==/UserScript==
 
--- edit-poster.user.js
+++ edit-poster.user.js
@@ -7,8 +7,8 @@
 // @match        *://shikimori.local/*
 // @match        *://shikimori.one/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shikimori.one
-// @downloadURL  https://github.com/shikimori/userscripts/blob/master/edit-poster.user.js
-// @updateURL    https://github.com/shikimori/userscripts/raw/refs/heads/master/edit-poster.user.js
+// @downloadURL  https://raw.githubusercontent.com/shikimori/userscripts/refs/heads/master/edit-poster.user.js
+// @updateURL    https://raw.githubusercontent.com/shikimori/userscripts/refs/heads/master/edit-poster.user.js
 // @grant        none
 // ==/UserScript==
 
